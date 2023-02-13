83f94419c357e50a8651375dd14c431bf510931c
berti <berti@epse.be>
2023-02-13 11:58:20 +0100

    cours 11 - invoicePage v1

---
+++ src/pages/InvoicePage.jsx
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react'
+import invoicesAPI from '../services/invoicesAPI'
+import Field from '../components/forms/Field'
+import customersAPI from '../services/customersAPI'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import Select from '../components/forms/Select'
+
+const InvoicePage = (props) => {
+    return ( 
+        <>
+        </> 
+    );
+}
+ 
+export default InvoicePage;
